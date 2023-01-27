6bdf51a880103e226727faaceaf3453879825aea
MMatyasBP2 <matyika123456@gmail.com>
2023-01-27 17:21:41 +0100

    Web project created

---
+++ Web/ZooApp/Connection.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using System.Diagnostics;
+
+namespace ZooApp
+{
+    public class Connection
+    {
+        public static IMongoClient Client { get; set; }
+        public static IMongoDatabase Db { get; set; }
+        public static string MongoConnection = "";
+        public static string MongoDatabase = "";
+
+        internal static void ConnectToMongoService()
+        {
+            try
+            {
+                Client = new MongoClient(MongoConnection);
+                Db = Client.GetDatabase(MongoDatabase);
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Web/ZooApp/Models/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace ZooApp.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Racial { get; set; }
+        public string Description { get; set; }
+        public string Habitat { get; set; }
+        public string User { get; set; }
+    }
+}
